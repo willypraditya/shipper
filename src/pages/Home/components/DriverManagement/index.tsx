08a9ab8da1b47ecb57c2edf14d52ce1247bb4ac9
Willy <w.praditya@yahoo.com>
2022-03-20 01:16:30 +0700

    view: move DriverManagement to component in Home

---
+++ src/pages/Home/components/DriverManagement/index.tsx
@@ -0,0 +1,42 @@
+import { ReactElement } from 'react';
+
+import { Button, Col, Input, Row, Typography } from 'antd';
+
+import { PlusOutlined, SearchOutlined } from '@ant-design/icons';
+
+import styles from './index.module.scss';
+
+const DriverManagement = (): ReactElement => (
+  <div className={styles.manage}>
+    <Row justify="space-between" align="middle" gutter={[12, 12]}>
+      <Col xs={24} lg={12} className={styles.manage__text}>
+        <Typography className={styles.manage__text__title}>DRIVER MANAGEMENT</Typography>
+        <Typography>Data driver yang bekerja dengan Anda.</Typography>
+      </Col>
+      <Col xs={24} lg={12} className={styles.manage__action}>
+        <Row gutter={[12, 12]}>
+          <Col xs={24} lg={12}>
+            <Input
+              size="large"
+              placeholder="Cari Driver"
+              className={styles.manage__search}
+              prefix={<SearchOutlined className={styles.manage__action__search__icon} />}
+            />
+          </Col>
+          <Col xs={24} lg={12}>
+            <Button
+              size="large"
+              block
+              className={styles.manage__action__addDriver}
+              icon={<PlusOutlined className={styles.manage__action__addDriver__icon} />}
+            >
+              TAMBAH DRIVER
+            </Button>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  </div>
+);
+
+export default DriverManagement;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,8 @@
 import { ReactElement } from 'react';
 
-import { Button, Col, Input, Row, Typography } from 'antd';
-
-import { PlusOutlined, PoweroffOutlined, SearchOutlined } from '@ant-design/icons';
-
 import MainLayout from '@pages/Components/MainLayout';
 
+import DriverManagement from './components/DriverManagement';
 import useHomeHooks from './hooks';
 import styles from './index.module.scss';
 
@@ -15,36 +12,7 @@ const Home = (): ReactElement => {
   return (
     <MainLayout>
       <div className={styles.home}>
-        <div className={styles.home__manage}>
-          <Row justify="space-between" align="middle" gutter={[12, 12]}>
-            <Col xs={24} lg={12} className={styles.home__manage__text}>
-              <Typography className={styles.home__manage__text__title}>DRIVER MANAGEMENT</Typography>
-              <Typography>Data driver yang bekerja dengan Anda.</Typography>
-            </Col>
-            <Col xs={24} lg={12} className={styles.home__manage__action}>
-              <Row gutter={[12, 12]}>
-                <Col xs={24} lg={12}>
-                  <Input
-                    size="large"
-                    placeholder="Cari Driver"
-                    className={styles.home__manage__search}
-                    prefix={<SearchOutlined className={styles.home__manage__action__search__icon} />}
-                  />
-                </Col>
-                <Col xs={24} lg={12}>
-                  <Button
-                    size="large"
-                    block
-                    className={styles.home__manage__action__addDriver}
-                    icon={<PlusOutlined className={styles.home__manage__action__addDriver__icon} />}
-                  >
-                    TAMBAH DRIVER
-                  </Button>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </div>
+        <DriverManagement />
       </div>
     </MainLayout>
   );
